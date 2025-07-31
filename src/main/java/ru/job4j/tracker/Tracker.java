cea8fced7c62973c815d205aa7454a6184dbfe7b
TimashovDmitry <timashov_dmitry@mail.ru>
2025-07-31 15:46:22 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -11,7 +12,34 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        int n = 0;
+        for (int index = 0; index <= size - 1; index++) {
+            if (items[index] != null) {
+                result[n] = items[index];
+                n++;
+            }
+            result = Arrays.copyOf(result, n + 1);
+        }
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int n = 0;
+        for (int index = 0; index < size - 1; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                result[n] = items[index];
+                n++;
+            }
+            result = Arrays.copyOf(result, n + 1);
+        }
+        return result;
+    }
+
+        public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
