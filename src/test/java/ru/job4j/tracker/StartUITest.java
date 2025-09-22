23a9f1c8e577a2f15e6eadbb3795e46f3db79aed
TimashovDmitry <timashov_dmitry@mail.ru>
2025-09-23 00:11:30 +0300

    12.1 Зависимость от System.out [#33568] Tests

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,7 +2,6 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-
 class StartUITest {
     @Test
     void whenCreateItem() {
